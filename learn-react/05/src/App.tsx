491b8bfe346870723f230bfe878a94aaa27dda5b
Ujin <tladbwls0430@gmail.com>
2025-04-16 16:10:50 +0900

    파일 추가

--- learn-react/05/src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <>
-      <h1 className="nanumpen text-3xl underline">App Component</h1>
-      <h1 className="nanumpen text-3xl underline font-bold">App Component</h1>
-    </>
-  );
-}
